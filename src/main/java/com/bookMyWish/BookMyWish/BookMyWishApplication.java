6a323e57a805b11bc576652bfda8ff4847c28906
amit-rajak <Welcoe@099>
2024-05-06 19:50:07 +0530

    Initial commit

---
+++ src/main/java/com/bookMyWish/BookMyWish/BookMyWishApplication.java
@@ -0,0 +1,13 @@
+package com.bookMyWish.BookMyWish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyWishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyWishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookMyWish/BookMyWish/BookMyWishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookMyWish.BookMyWish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyWishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
